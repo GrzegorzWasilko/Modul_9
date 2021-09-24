69c81fdaa13053d75c0d8fa68a15469928688764
Grzegorz Wasilko <wasilko1@o2.pl>
2021-09-24 11:24:49 +0200

    DONE

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import json
-import requests   #  python -m pip install requests <<---install
+import requests  
 from flask import Flask, render_template, request, redirect
-import os # do obsługi plików, ładowania i wyładowywania
+import os 
 import csv
 
 app = Flask (__name__)
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data=response.json()#data to lista całego request jako słownik
-rates = data[0].get('rates')# wziełem rates/(lista słowników) bo rates to key
-#print(f'\nzawartość rates to: {rates}\n')#lista słowników kursów walut
+data=response.json()
+rates = data[0].get('rates')
+
 #===============================================================================
 def get_codes():
     codes = []
@@ -17,10 +17,8 @@ def get_codes():
         codes.append(data.get('code'))
     return sorted(codes)
 
-codes = get_codes()#codes to lista ['AUD', 'CAD', 'CHF', 'CZK', ....
+codes = get_codes()
 
-#print(f'typ codes to: {type(codes)}')
-print(f'zawartość codes to: {codes}\n')
 #==============================================================================
 columns=['currency','code','bid','ask']
 #==============================================================================
@@ -34,13 +32,11 @@ with open ("rates.csv") as filecsv:
     writer = csv.reader(filecsv,delimiter=';')
     for i in writer:
         pass
-       #print(f'i to == {i}')
-
 #===============================================================================
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
-#===============================================================================
+
 #================Calculator=====================================================
 
 @app.route('/calculator', methods=["GET", "POST"])
@@ -49,24 +45,14 @@ def calculator():
         return render_template("calculator.html",rates=rates)
 
     if request.method == "POST":
-        print(f'\nprint request ==========>> {request.form}')
         form =request.form #całość form z calculator
-        print(f'\nform to  ==========>> {form}')
-
         code =form.get('rates',type=float) #skrót waluty
-        
-        print(f'\ncode  linia 55 to  ==========>> {code}')
-        print (type(code))
         amount=form.get('amount',type=float)#ilość waluty
-        print(f'\namount to  ==========>> {amount}')
         price = code*amount
-        print(f'\nprice to  ==========>> {price}')
-        # communicat=print (f'% 2 f' % price )
+        # communicat=print ('% 2 f' % price )
         print(f'\nTo będzie kosztować {price } polskich złotych\n')
         return render_template("/response.html",price = price )
 #===============================================================================
-
-
 if __name__ == "__main__":
     app.run(debug=True)
 
